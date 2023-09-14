0e036329dfa75d2a16ebf8972e8cc22a330e8463
sabah <sabah72224@gmail.com>
2023-09-15 01:34:35 +0600

    gege

--- FTP/FTP_1905118/src/Server/Server.java
+++ FTP/FTP_1905118/src/Server/Server.java
@@ -5,36 +5,39 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    private static DataOutputStream dataOutputStream = null;
-    private static DataInputStream dataInputStream = null;
+    private DataOutputStream dataOutputStream = null;
+    private DataInputStream dataInputStream = null;
 
     public static void main(String[] args) {
-        try (ServerSocket serverSocket = new ServerSocket(6600)) {
-            System.out.println("listening to port:6600");
-            Socket clientSocket = serverSocket.accept();
-            System.out.println(clientSocket + " connected\n");
+        while (true) {
+            try (ServerSocket serverSocket = new ServerSocket(6600)) {
+                System.out.println("listening to port:6600");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println(clientSocket + " connected\n");
 
-            dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
-            dataInputStream = new DataInputStream(clientSocket.getInputStream());
+                dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+                dataInputStream = new DataInputStream(clientSocket.getInputStream());
 
-            String message;
-            while (true) {
-                try {
-                    message = dataInputStream.readUTF();
-                    System.out.println(message);
-                    if (message.equalsIgnoreCase("exit()")) break;
+                String message;
+                while (true) {
+                    try {
+                        message = dataInputStream.readUTF();
+                        System.out.println(message);
+                        if (message.equalsIgnoreCase("exit()")) {
+                            clientSocket.close();
+                            dataInputStream.close();
+                            dataOutputStream.close();
+                            break;
+                        }
+                    } catch (Exception e) {
+                        clientSocket.close();
+                        dataInputStream.close();
+                        dataOutputStream.close();
+                    }
                 }
-                catch (Exception e) {
-                    clientSocket.close();
-                    dataInputStream.close();
-                    dataOutputStream.close();
-                    e.printStackTrace();
-                    break;
-                }
+
+            } catch (Exception e) {
             }
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
