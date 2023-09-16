1b347d2c9340bbce6c32b283fd04722a386c77b6
sabahcsxgawd <sabah72224@gmail.com>
2023-09-16 12:05:40 +0600

    gege

--- FTP/FTP_1905118/scripts/client.sh
+++ FTP/FTP_1905118/scripts/client.sh
@@ -1,7 +1,8 @@
 #! /usr/bin/bash
 
 clear
-outDirPath="../out/production/FTP_1905118"
+outDirPath="./out/production/FTP_1905118"
 
 #TODO rm Downloads of Clients
+cd ..
 gnome-terminal --title="Client" -- java -cp "${outDirPath}" "Client/Client"
--- FTP/FTP_1905118/scripts/server.sh
+++ FTP/FTP_1905118/scripts/server.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/bash
 
 clear
-outDirPath="../out/production/FTP_1905118"
+outDirPath="./out/production/FTP_1905118"
 #rm -r "${serverSrcDirPath}/ClientDirs/"*
+./build.sh
+cd ..
 gnome-terminal --title="Server" -- java -cp "${outDirPath}" "Server/Server"
